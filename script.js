0ab6b67be997dfa6c7a7287f7505cce47436e6b4
BYREDDI RAMANA BABU <127107319+BYREDDI@users.noreply.github.com>
2023-09-19 15:54:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	str.sort((a,b)=>a-b);
+	console.log(str);
+	for(let i=0;i<str.length-2;i++)
+		{
+			let j=i+1,k=i+2;
+		}
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
